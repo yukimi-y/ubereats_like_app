33564127fbd2c228975f658f687d259388abc857
yukimi.y <yyy.yukimi@gmail.com>
2021-04-15 08:58:06 +0900

    restaurant.rb修正し，DBへデータ追加

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,7 +3,7 @@ class Restaurant < ApplicationRecord
   has_many :line_foods, through: :foods
   belongs_to :order, optional: true
 
-  validates :name, :fee, :time_required, presemce: true
-  validates :name, length: { miaximum: 30 }
+  validates :name, :fee, :time_required, presence: true
+  validates :name, length: { maximum: 30 }
   validates :fee, numericality: { greater_than: 0 }
 end
